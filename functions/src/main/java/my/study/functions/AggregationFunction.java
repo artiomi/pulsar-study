4e21987ebf2a75a3e5051f70d1969be4872616d6
Artiom Iurcovschi <artiom.08@gmail.com>
2023-05-30 13:46:06 +0300

    add log during function execution

--- functions/src/main/java/my/study/functions/AggregationFunction.java
+++ functions/src/main/java/my/study/functions/AggregationFunction.java
@@ -4,13 +4,17 @@ import org.apache.pulsar.client.api.Schema;
 import org.apache.pulsar.functions.api.Context;
 import org.apache.pulsar.functions.api.Function;
 import org.apache.pulsar.functions.api.Record;
+import org.slf4j.Logger;
 
 public class AggregationFunction implements Function<String, Record<Counter>> {
 
   @Override
   public Record<Counter> process(String input, Context context) {
+    Logger log = context.getLogger();
     context.incrCounter(input, 1);
     long counter = context.getCounter(input);
+
+    log.info("Input processed:[{}]", input);
 
     return context.newOutputRecordBuilder(Schema.JSON(Counter.class))
         .value(new Counter(input, counter))
